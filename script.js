3842fac9ca7c28c4a3a4b640f5c18ae85cb3b074
Milena Valença da Rocha <149966687+milenavlcr@users.noreply.github.com>
2024-03-28 19:00:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ msg.innerHTML = `Agora são ${hora} horas e ${minute} minutos` // *${seconds} se
 if(hora >= 5 && hora < 12) {
 // Bom dia!
  img.src = 'manhã.png'
- document.body.style.background = '#FE8D00'
+ document.body.style.background = '#FDAF00'
 } else if (hora >= 12 && hora <= 18) {
     img.src = 'tarde.png'
     // Boa tarde!
-    document.body.style.background = '#F1C9AD'
-} else if (hora >= 18 && hora <= 23) { 
+    document.body.style.background = '#954A14'
+} if (hora >= 18 && hora <= 23) { 
     // Boa noite!
     img.src = 'noite.png'
     document.body.style.background = '#293535'
